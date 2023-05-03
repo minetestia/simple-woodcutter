eab4b91f2cc9dccf7dce2fba4a0699fe25ca4916
luarocks <luarocks@onionmail.org>
2023-05-03 04:18:01 +0300

    Shortened one line :)

--- init.lua
+++ init.lua
@@ -2,8 +2,7 @@ local max_distance, max_radius, delay = 20, 4, 0.1
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
   if not minetest.registered_nodes[oldnode.name].groups.tree then return end
-  local groups = digger:get_wielded_item():get_definition().groups
-  if not groups or not groups.axe then return end
+  if not digger:get_wielded_item():get_definition().groups.axe then return end
   if digger:get_hp() == 0 then return end
   minetest.after(delay, function()
     pos = minetest.find_node_near(pos, max_radius, oldnode.name)
