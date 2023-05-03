79db265352d8ebafc665ea642885103fc274f494
luarocks <luarocks@onionmail.org>
2023-05-03 03:41:54 +0300

    Fix multiplayer bug

--- init.lua
+++ init.lua
@@ -5,11 +5,12 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
   local groups = digger:get_wielded_item():get_definition().groups
   if not groups or not groups.axe then return end
   if digger:get_hp() == 0 then return end
-  if not minetest.get_player_by_name(digger:get_player_name()) then return end
   minetest.after(delay, function()
     pos = minetest.find_node_near(pos, max_radius, oldnode.name)
     if not pos then return end
-    if pos:distance(digger:get_pos()) > max_distance then return end
+    local digger_pos = digger:get_pos()
+    if not digger_pos then return end
+    if pos:distance(digger_pos) > max_distance then return end
     minetest.node_dig(pos, minetest.get_node(pos), digger)
   end)
 end)
