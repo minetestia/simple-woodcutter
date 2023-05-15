dd41aff34abcceabf2f47d81f976bfc64ced94a4
luarocks <luarocks@onionmail.org>
2023-05-15 11:19:51 +0300

    Refactor

--- init.lua
+++ init.lua
@@ -4,9 +4,12 @@ local max_radius = tonumber(ms:get "simple_woodcutter.max_radius") or 1
 local delay = tonumber(ms:get "simple_woodcutter.delay") or 0.01
 local S = mt.get_translator(mt.get_current_modname())
 local privilege = { description = S "Player can fell trees quickly." }
-mt.register_privilege("lumberjack", privilege)
 
-local function chop_around(pos, oldnode, digger)
+local function chop_recursive(pos, oldnode, digger)
+  if not digger:is_player() then return end
+  if not mt.registered_nodes[oldnode.name].groups.tree then return end
+  if not digger:get_wielded_item():get_definition().groups.axe then return end
+  if digger:get_hp() == 0 then return end
   local d = delay
   if not mt.check_player_privs(digger, "lumberjack") then d = d * 100 end
   mt.after(d, function()
@@ -16,15 +19,9 @@ local function chop_around(pos, oldnode, digger)
     if not digger_pos then return end
     if pos:distance(digger_pos) > max_distance then return end
     mt.node_dig(next_pos, mt.get_node(next_pos), digger)
-    chop_around(pos, oldnode, digger)
+    chop_recursive(pos, oldnode, digger)
   end)
 end
 
----@param digger mt.PlayerObjectRef
-mt.register_on_dignode(function(pos, oldnode, digger)
-  if not digger:is_player() then return end
-  if not mt.registered_nodes[oldnode.name].groups.tree then return end
-  if not digger:get_wielded_item():get_definition().groups.axe then return end
-  if digger:get_hp() == 0 then return end
-  chop_around(pos, oldnode, digger)
-end)
+mt.register_privilege("lumberjack", privilege)
+mt.register_on_dignode(chop_recursive)
